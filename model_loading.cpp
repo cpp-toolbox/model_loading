c5951ea14e40d47027eefa5201e3899cbc818827
cuppajoeman <ccn@cuppajoeman.com>
2024-08-01 17:58:41 -0400

    updat model loading

--- model_loading.cpp
+++ model_loading.cpp
@@ -1,115 +1,77 @@
+
+#include <spdlog/spdlog.h>
+#include <assimp/postprocess.h>
+
 #include "model_loading.hpp"
+#include "../../graphics/textured_model_loading/sbpt_generated_includes.hpp"
 
-#include <iostream>
+Model ModelLoader::load_model(const std::string &path) {
 
-#include "assimp/Importer.hpp"
-#include "assimp/postprocess.h"
-#include <utility>
-
-/**
- * overview:
- * initialization:
- *  first we iterate through the entire assimp imported object recursively
- *  we store all the images we found in the base model, this is the initialization phase.
- *
- */
-
-Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices) {
-    this->vertices = std::move(vertices);
-    this->indices = std::move(indices);
-};
-
-/**
- * \brief loads a 3d model into wrapper structure
- *
- * \pre the shader files specified exist and are at this file path
- * \todo have a resource directory so that we don't have to specify a long relative path
- *
- * @param path the path to the model we want to load
- */
-Model::Model(std::string path) { this->load_model(std::move(path)); }
-
-/**
- * notes:
- * 	- this function is the entry point to the initialization process
- * 	- although this function looks short and simple, the call to process_node
- * 	is recursive and does all the work of parsing assimp's structure
- */
-void Model::load_model(std::string path) {
-    Assimp::Importer importer;
-    const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate);
-    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
-        std::cout << "ERROR::ASIMP::" << importer.GetErrorString() << std::endl;
-        return;
-    }
-    this->directory = path.substr(0, path.find_last_of("/"));
-    printf("starting to process nodes \n");
-    this->process_node(scene->mRootNode, scene);
-    printf("processed all nodes\n");
-};
-
-/**
- * notes:
- * - recall that a node may contains a collection of meshes, and
- * also children nodes
- * - this function is guarenteed to terminate because modellers
- * create strctures made up of finitely many nodes and meshes.
- * - this can be thought of the same way that each folder in blender can have individual meshes inside or more
- * folders (I believe a folder is called a collection) and just represents arbitrary nesting
- */
-void Model::process_node(aiNode *node, const aiScene *scene) {
-    printf("stared processing meshes\n");
-    for (unsigned int i = 0; i < node->mNumMeshes; i++) {
-        aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+    auto process_step = [this](aiMesh *mesh, const aiScene *scene) {
         this->meshes.push_back(this->process_mesh(mesh, scene));
-    }
-    printf("finished processing meshes\n");
-    for (unsigned int i = 0; i < node->mNumChildren; i++) {
-        process_node(node->mChildren[i], scene);
-    }
+    };
+
+    auto recursively_process_nodes = this->recursively_process_nodes_closure(process_step);
+
+    this->call_function_with_assimp_importer_context(path, [&](auto root, auto scene) {
+        spdlog::get(Systems::asset_loading)->info("starting to process nodes");
+        recursively_process_nodes(scene->mRootNode, scene);
+        spdlog::get(Systems::asset_loading)->info("processed all nodes");
+    });
+
+    return Model(this->meshes);
+};
+
+void ModelLoader::process_function(aiMesh *mesh, const aiScene *scene) {}
+/**
+ * /note this function is guaranteed to terminate because all models are finite and non-cyclic
+ */
+std::function<void(aiNode *, const aiScene *)>
+ModelLoader::recursively_process_nodes_closure(std::function<void(aiMesh *, const aiScene *)> process_function) {
+    std::function<void(aiNode * node, const aiScene *scene)> recursively_process_nodes = [&](aiNode *node,
+                                                                                             const aiScene *scene) {
+        spdlog::get(Systems::asset_loading)->info("stared processing meshes");
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+            unsigned int mesh_index = node->mMeshes[i];
+            aiMesh *mesh = scene->mMeshes[mesh_index];
+            process_function(mesh, scene);
+        }
+        spdlog::get(Systems::asset_loading)->info("finished processing meshes");
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
+            recursively_process_nodes(node->mChildren[i], scene);
+        }
+    };
+    return recursively_process_nodes;
+};
+
+Mesh ModelLoader::process_mesh(aiMesh *mesh, const aiScene *scene) {
+    std::vector<glm::vec3> vertices = this->process_mesh_vertex_positions(mesh);
+    std::vector<unsigned int> indices = this->process_mesh_indices(mesh);
+    return {vertices, indices};
 };
 
 glm::vec3 assimp_to_glm_3d_vector(aiVector3D assimp_vector) {
     return {assimp_vector.x, assimp_vector.y, assimp_vector.z};
 }
 
-std::vector<Vertex> Model::process_mesh_vertices(aiMesh *mesh) {
-    std::vector<Vertex> vertices;
-
-    bool mesh_has_texture_coordinates = mesh->mTextureCoords[0] != nullptr;
-    printf("This mesh has %d vertices\n", mesh->mNumVertices);
+std::vector<glm::vec3> ModelLoader::process_mesh_vertex_positions(aiMesh *mesh) {
+    std::vector<glm::vec3> vertices;
+    spdlog::get(Systems::asset_loading)->info("This mesh has {} vertex_positions", mesh->mNumVertices);
     for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
-        Vertex vertex;
-        vertex.position = assimp_to_glm_3d_vector(mesh->mVertices[i]);
+        glm::vec3 vertex = {assimp_to_glm_3d_vector(mesh->mVertices[i])};
         vertices.push_back(vertex);
     }
     return vertices;
 }
 
-/**
- * \brief given a mesh iterate through each face storing all indices of each vertex on that face
- */
-std::vector<unsigned int> Model::process_mesh_indices(aiMesh *mesh) {
+std::vector<unsigned int> ModelLoader::process_mesh_indices(aiMesh *mesh) {
     std::vector<unsigned int> indices;
     for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
         aiFace face = mesh->mFaces[i];
-
         assert(face.mNumIndices == 3); // if this is false we are not working with triangles
-
         for (unsigned int j = 0; j < face.mNumIndices; j++) {
             indices.push_back(face.mIndices[j]);
         }
     }
     return indices;
 }
-
-/**
- * description
- * 	takes in assimp's interpretation of a mesh, and then parses it
- * 	into our version of a mesh
- */
-Mesh Model::process_mesh(aiMesh *mesh, const aiScene *scene) {
-    std::vector<Vertex> vertices = this->process_mesh_vertices(mesh);
-    std::vector<unsigned int> indices = this->process_mesh_indices(mesh);
-    return {vertices, indices};
-};
--- model_loading.hpp
+++ model_loading.hpp
@@ -3,56 +3,52 @@
 
 #include "glm/glm.hpp"
 #include <string>
+#include <utility>
 #include <vector>
+#include <GLFW/glfw3.h>
 #include <assimp/scene.h>
+#include <functional>
+
+glm::vec3 assimp_to_glm_3d_vector(aiVector3D assimp_vector);
 
 /**
- * description:
- * 	a vertex in the context of a 3d model
- */
-struct Vertex {
-    glm::vec3 position;
-};
-
-/**
- * description:
- * 	a collection of vertices along with an ordering of how to traverse them
- *
- * example:
- * 	a simple hamburger 3d model might have 3 meshes, one for the bottom bun,
- * the burger and the top bun
- *
+ * \brief A mesh is a collection of vertex_positions and indices which describe the order in which the vertex_positions
+ * should be traversed in order to draw it using the standard interpretation of 3 sequential vertex_positions as a
+ * triangle.
  */
 class Mesh {
   public:
-    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices);
-    std::vector<Vertex> vertices;
+    Mesh(std::vector<glm::vec3> &vertex_positions, std::vector<unsigned int> &indices)
+        : vertex_positions(vertex_positions), indices(indices){};
     std::vector<unsigned int> indices;
+    std::vector<glm::vec3> vertex_positions;
 };
 
 /**
- * description:
- *  a collection of meshes, provides the ability to load from file
- * example:
- *  a 3d model that represents the entire burger as in the mesh example
+ * /brief A model is a collection of Meshes
  */
 class Model {
   public:
-    // Should the shader be stored inside of the model class? Unique to each
-    // shader?
-    Model(std::string path);
+    explicit Model(std::vector<Mesh> meshes) : meshes(std::move(meshes)){};
     std::vector<Mesh> meshes;
-
-  private:
-    void load_model(std::string path);
-
-    std::string directory;
-
-    void process_node(aiNode *node, const aiScene *scene);
-
-    std::vector<Vertex> process_mesh_vertices(aiMesh *mesh);
-    std::vector<unsigned int> process_mesh_indices(aiMesh *mesh);
-    Mesh process_mesh(aiMesh *mesh, const aiScene *scene);
 };
+
+class ModelLoader {
+    std::vector<Mesh> meshes;
+    std::string path_to_assets_directory = "assets";
+    std::string directory;
+    void process_function(aiMesh *mesh, const aiScene *scene);
+    std::vector<glm::vec3> process_mesh_vertex_positions(aiMesh *mesh);
+    std::vector<unsigned int> process_mesh_indices(aiMesh *mesh);
+    Model load_model(const std::string &path);
+
+  protected:
+    Mesh process_mesh(aiMesh *mesh, const aiScene *scene);
+    static std::function<void(aiNode *, const aiScene *)>
+    recursively_process_nodes_closure(std::function<void(aiMesh *, const aiScene *)> process_function);
+    template <typename func> void call_function_with_assimp_importer_context(const std::string &path, func fn);
+};
+
+#include "model_loading.tpp"
 
 #endif // MODEL_LOADING_HPP
